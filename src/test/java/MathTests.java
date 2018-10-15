418bbcf4b46208f86fc9a564ab3f1f4b00bc70ce
Adrian Dilita <adrian.dilita@mindit.io>
2018-10-15 15:54:22 +0300

    beginning of unit tests

---
+++ src/test/java/MathTests.java
@@ -0,0 +1,17 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MathTests {
+    @Test
+    public void add_TwoPlusTwo_ReturnsFour() {
+        //Arrange
+        final int expected = 4;
+
+        //Act
+        final int actual = Math.add(2, 2);
+
+        //Assert
+        Assert.assertEquals(actual, expected);
+    }
+
+}
